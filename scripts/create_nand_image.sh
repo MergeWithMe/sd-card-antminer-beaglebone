904b92dd21acb4b6d15cde705f7d6c8eea1f0c23
Merge With Me <mergewithme@protonmail.com>
2025-03-26 03:15:05 +0100

    gitignore

--- scripts/create_nand_image.sh
+++ scripts/create_nand_image.sh
@@ -8,7 +8,7 @@ NAND_TOTAL_SIZE=$((0x8000000))  # 128MB
 
 # Create empty NAND image
 echo "Creating NAND image of size $NAND_TOTAL_SIZE bytes..."
-dd if=/dev/zero of=$NAND_IMAGE bs=1 count=0 seek=$NAND_TOTAL_SIZE
+dd if=/dev/zero of=$NAND_IMAGE count=1 bs=$NAND_TOTAL_SIZE
 
 # Define partitions (offset, size)
 PARTITIONS=(
@@ -17,29 +17,29 @@ PARTITIONS=(
     "spl_backup2 0x40000 0x20000 ./u-boot-antminer-beaglebone/MLO"
     "spl_backup3 0x60000 0x20000 ./u-boot-antminer-beaglebone/MLO"
     "u-boot 0x80000 0x1c0000 ./u-boot-antminer-beaglebone/u-boot.img"
-    "bootenv 0x240000 0x20000 bootenv.bin"
+    "bootenv 0x240000 0x20000 ./out/bootenv.bin"
     "fdt 0x260000 0x20000 ./fat/extlinux/am335x-boneblack-blackmainer.dtb"
     "kernel 0x280000 0x500000 ./fat/zImage"
     "root 0x800000 0x1400000 ./out/initramfs.bin.SD"
-    "config 0x1c00000 0x1400000 config.bin"
+    "config 0x1c00000 0x1400000 ./out/config.ext4"
     "fpgabit 0x3000000 0x5000000 ./out/fpga.ext4"
 )
 
 echo "Packing root filesystem into initramfs ..."
 cd fs  # Go into the root filesystem folder
 # Create the initramfs.cpio.gz
-find . | cpio -o -H newc | gzip > ../initramfs.cpio.gz
+find . | cpio -o -H newc | gzip > ../out/initramfs.cpio.gz
 cd ..  # Go back to the parent directory
 # Create the U-Boot initramfs image (uImage)
-mkimage -A arm -O linux -T ramdisk -C gzip -d initramfs.cpio.gz initramfs.bin.SD
+mkimage -A arm -O linux -T ramdisk -C gzip -d ./out/initramfs.cpio.gz ./out/initramfs.bin.SD
 
 echo "Creating /fpgabit partition"
 # Define size (0x5000000 = 80MB)
 SIZE=$((0x5000000))
 # Create an empty file of the correct size
-dd if=/dev/zero of=./out/fpga.ext4 bs=1 count=0 seek=$SIZE
+dd if=/dev/zero of=./out/fpga.ext4 bs=$SIZE count=1
 # Format it as ext4
-mkfs.ext4 ./out/fpga.ext4
+./legacy_tools/mkfs.ext4 ./out/fpga.ext4
 # Create a temporary mount point
 mkdir -p mnt_fpga
 # Mount the image
@@ -52,17 +52,38 @@ sudo umount mnt_fpga
 # Remove the temporary mount point
 rmdir mnt_fpga
 
+echo "Creating /config partition"
+SIZE=$((0x1400000))
+# Create an empty file of the correct size
+dd if=/dev/zero of=./out/config.ext4 bs=$SIZE count=1
+# Format it as ext4
+./legacy_tools/mkfs.ext4 ./out/config.ext4
+# Create a temporary mount point
+mkdir -p mnt_config
+# Mount the image
+sudo mount -o loop ./out/config.ext4 mnt_config
+# Copy all bitstreams/* into the filesystem
+sudo cp -r configs/* mnt_config/
+# Sync and unmount
+sync
+sudo umount mnt_config
+# Remove the temporary mount point
+rmdir mnt_config
+
+echo "Creating bootenv.bin image ..."
+mkenvimage -s 0x20000 -o ./out/bootenv.bin ./bootenv/bootenv.txt
+
 echo "Writing partition image ..."
 # Create partitions
 for entry in "${PARTITIONS[@]}"; do
     set -- $entry
     NAME=$1
-    OFFSET=$2
-    SIZE=$3
+    OFFSET=$(($2))
+    SIZE=$(($3))
     FILENAME=$4
 
     echo "Creating partition '$NAME' at offset $OFFSET with size $SIZE and file $FILENAME ..."
-    dd if=$FILENAME bs=1 count=$SIZE seek=$OFFSET of=$NAND_IMAGE conv=notrunc
+    dd if=$FILENAME bs=512 seek=$((OFFSET / 512)) of=$NAND_IMAGE count=$((SIZE / 512)) conv=notrunc
 done
 
 
